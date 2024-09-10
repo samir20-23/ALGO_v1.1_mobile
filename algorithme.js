19c91f5230f77825834ef60282f751be8b26e96f
samir aoulad amar <144660881+samir20-23@users.noreply.github.com>
2024-09-10 23:53:40 +0100

    Update algorithme.js

--- algorithme.js
+++ algorithme.js
@@ -55,3 +55,81 @@ b++
     a++
 }
 console.log(array);
+
+//                               methode number 4
+// let array = [6,4,7,2,3,1,5,8,10,9];
+// let tm , im ; 
+// let count = 0;
+// do{
+//      if(array[count] !== undefined){
+//         count++;
+//      }else{
+//           break;
+//      }
+// }
+// while(true);
+// for(a=0;a<count;a++){
+// for(b=a;b<count;b++){
+//      //
+//      function iff(condition,trueBlock,falseBlock){
+// condition ? trueBlock() : falseBlock();
+//      }
+//      iff(array[a]<array[b],function(){tm = array[a];
+//           array[a] = array[b];
+//           array[b] = tm;},function(){});
+//      //
+ 
+// }
+// }
+// console.log(array);
+//                               methode number 5
+// let arr = [6,5,7,4,8,3,2,1];
+// let tm ;
+// let cout = 0;
+// do{
+//  if(arr[cout] !== undefined){
+//      cout++;
+//  }else{
+//      break;
+//  }
+// }
+// while(true);
+// for(a=0;a<cout;a++){
+// for(b=a;b<cout;b++){
+//     function iff(condition,trueBlock,falseBlock){
+// condition ? trueBlock() : falseBlock();
+//     }
+//     iff(arr[a]<arr[b],()=>{
+//      tm = arr[a];
+//      arr[a]=arr[b];
+//      arr[b]=tm;
+//     })
+// }
+// }
+// console.log(arr);
+
+//                               methode number 6
+// let ar = [5,6,4,7,2,8,1,3];
+// let tm ;
+// let len;
+// let count = 0;
+// while(ar[count] !== undefined){
+// count++;
+// break;
+// }
+// len= count;
+// for ( a = 0; a <len; a++) {
+//      for (let b = a; b < len; b++) { 
+//           function iff(condition,trueBlock,falseBlock){
+// condition ? trueBlock() : falseBlock();
+//           }     
+//           iff(ar[a]<ar[b],()=>{
+//                tm =ar[a];
+//                ar[a]=ar[b];
+//                ar[b] =tm;
+//           },()=>{})
+//      }
+     
+// }
+// console.log(ar);
+
